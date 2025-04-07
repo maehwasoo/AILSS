d1363faa0128ce8a96a952bb4b073a80b7fdd236
Kyoungho Eom <kheom0512@gmail.com>
2025-04-07 22:49:05 +0900

    fix: 글로벌 그래프 설정 조정
    노드 및 선 크기, 밀어내기 강도, 연결 강도와 거리를 조정

--- src/modules/maintenance/utils/graph/global/defaultGlobalGraph.ts
+++ src/modules/maintenance/utils/graph/global/defaultGlobalGraph.ts
@@ -88,13 +88,13 @@ export const DEFAULT_GLOBAL_GRAPH = {
   "collapse-display": false,
   "showArrow": true,
   "textFadeMultiplier": 0,
-  "nodeSizeMultiplier": 1,
-  "lineSizeMultiplier": 0.8,
+  "nodeSizeMultiplier": 1.25,
+  "lineSizeMultiplier": 0.55,
   "collapse-forces": false,
   "centerStrength": 0.25,
-  "repelStrength": 8.5,
-  "linkStrength": 0.9,
-  "linkDistance": 35,
+  "repelStrength": 8.25,
+  "linkStrength": 0.75,
+  "linkDistance": 85,
   "scale": 0.2,
   "close": true
 } as const;
